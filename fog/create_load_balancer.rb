2d57d7b997e781bf8078d8600d3e755e3616083f
Jonathan Jeffus <jjeffus@gmail.com>
2013-06-17 11:13:44 -0700

    load balancing and server creation

--- fog/create_load_balancer.rb
+++ fog/create_load_balancer.rb
@@ -2,11 +2,11 @@
 
 require 'fog'
 require 'awesome_print'
+require 'json'
 
 # Fog.mock!
 
 domain = ARGV.shift
-cert_id = ARGV.shift
 
 aws_credentials = YAML.load(File.open("creds.yml"))
 
@@ -14,20 +14,45 @@ api        = Fog::AWS::ELB.new(aws_credentials)
 iam        = Fog::AWS::IAM.new(aws_credentials)
 result = iam.list_server_certificates({'PathPrefix' => '/'})
 
+arn = nil
 result.body['Certificates'].each do |cert|
-  ap cert
+  if cert['ServerCertificateName'] == domain
+    ap cert
+    arn = cert['Arn']
+  end
 end
 
-__END__
+if arn.nil?
+  STDERR.puts "Certificate ID not found for: #{domain}"
+  exit 1
+end
 
-lb = api.load_balancers.create(:id => domain, 'ListenerDescriptions' => [{
-    'Listener' => {
-      'Protocol' => 'HTTPS',
-      'LoadBalancerPort' => 443,
-      'InstancePort' => 80,
-      'InstanceProtocol' => 'HTTP',
-      'SSLCertificateId' => cert_id
-    }
-}])
+STDERR.puts "Got certificate id: #{arn}"
 
-ap lb
+lb = nil
+api.load_balancers.each do |load_balancer|
+  if load_balancer.id == domain
+    lb = load_balancer
+    STDERR.puts "Got lb: "
+    ap lb
+  end
+end
+
+if lb == nil
+  availability_zones = ['us-east-1a']
+  lb = api.load_balancers.create(:id => domain.gsub(/[^a-zA-Z0-9]/, ''),
+    :availability_zones => availability_zones,
+    'ListenerDescriptions' => [{
+      'Listener' => {
+        'Protocol'         => 'HTTPS',
+        'LoadBalancerPort' => 443,
+        'InstancePort'     => 80,
+        'InstanceProtocol' => 'HTTP',
+        'SSLCertificateId' => arn
+      }
+  }])
+end
+
+server = JSON.parse(File.read('server.json'))
+
+lb.register_instances(server['id'])
